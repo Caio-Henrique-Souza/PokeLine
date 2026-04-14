58a146f949c89239ac6cfd874a92bf1a25ab6cdb
Caio Henrique Souza <detetives.l.c@gmail.com>
2026-04-14 15:05:14 -0300

    Extração detalhada base

--- src/extract/extract.py
+++ src/extract/extract.py
@@ -20,20 +20,30 @@ def extrair_pokemons(quantidade=151):
     else:
         print(f"Erro ao acessar a API: {resposta.status_code}")
 
-def extrair_especie(quantidade=151):
-    url = f"https://pokeapi.co/api/v2/pokemon-species?limit={quantidade}"
+def extrair_detalhes_pokemons():
+    origem = "data/raw/pokemons_base_raw.json"
     destino = "data/raw"
-    resposta = requests.get(url)
-
+    
     os.makedirs(destino, exist_ok=True)
 
-    if resposta.status_code == 200: #se não der erro na API
-        dados = resposta.json()
+    with open(origem, 'r') as f:
+        dados = json.load(f)
 
-        caminho_arquivo = f"{destino}/pokemons_especies_raw.json"
-        with open(caminho_arquivo, 'w') as f:
-            json.dump(dados, f, indent= 4)
+    detalhes = []
 
-        print(f"Sucesso! {quantidade} espécies Salvos em {caminho_arquivo}")
-    else:
-        print(f"Erro ao acessar a API: {resposta.status_code}")
+    for pokemon in dados["results"]:
+        url = pokemon["url"]
+        
+        resposta = requests.get(url)
+        
+        if resposta.status_code == 200:
+            detalhes.append(resposta.json())
+        else:
+            print(f"Erro ao acessar {url}: {resposta.status_code}")
+
+    caminho_arquivo = f"{destino}/pokemons_detalhes_raw.json"
+    
+    with open(caminho_arquivo, 'w') as f:
+        json.dump(detalhes, f, indent=4)
+
+    print(f"Sucesso! Detalhes salvos em {caminho_arquivo}")
--- src/main/main.py
+++ src/main/main.py
@@ -1,15 +1,13 @@
-from src.extract.extract import extrair_pokemons, extrair_especie
-
+from src.extract.extract import extrair_pokemons, extrair_detalhes_pokemons
 def run_pipeline():
     
     print("--- INICIANDO PIPELINE ---")
     
     print("Passo 1: Extraindo dados e gerando JSON")
-    extrair_pokemons(151)
+    extrair_pokemons(1)
 
-    print("Passo 2: Extraindo especies e gerando JSON")
-    extrair_especie(151)
-
+    print("Passo 2: Extraindo detalhes da base")
+    extrair_detalhes_pokemons()
 
     print("--- PIPELINE FINALIZADA... POR ENQUANTO ---")
 
