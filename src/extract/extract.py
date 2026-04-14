96e25d48b9d5f2fbcf49c43ebff19e31dcd00889
Caio Henrique Souza <detetives.l.c@gmail.com>
2026-04-14 13:02:29 -0300

    Nova extração + espécies

--- src/extract/extract.py
+++ src/extract/extract.py
@@ -12,10 +12,28 @@ def extrair_pokemons(quantidade=151):
     if resposta.status_code == 200: #se não der erro na API
         dados = resposta.json()
 
-        caminho_arquivo = f"{destino}/pokedex_raw.json"
+        caminho_arquivo = f"{destino}/pokemons_base_raw.json"
         with open(caminho_arquivo, 'w') as f:
             json.dump(dados, f, indent= 4)
 
         print(f"Sucesso! {quantidade} Pokémons Salvos em {caminho_arquivo}")
+    else:
+        print(f"Erro ao acessar a API: {resposta.status_code}")
+
+def extrair_especie(quantidade=151):
+    url = f"https://pokeapi.co/api/v2/pokemon-species?limit={quantidade}"
+    destino = "data/raw"
+    resposta = requests.get(url)
+
+    os.makedirs(destino, exist_ok=True)
+
+    if resposta.status_code == 200: #se não der erro na API
+        dados = resposta.json()
+
+        caminho_arquivo = f"{destino}/pokemons_especies_raw.json"
+        with open(caminho_arquivo, 'w') as f:
+            json.dump(dados, f, indent= 4)
+
+        print(f"Sucesso! {quantidade} espécies Salvos em {caminho_arquivo}")
     else:
         print(f"Erro ao acessar a API: {resposta.status_code}")
--- src/main/main.py
+++ src/main/main.py
@@ -1,4 +1,4 @@
-from src.extract.extract import extrair_pokemons
+from src.extract.extract import extrair_pokemons, extrair_especie
 
 def run_pipeline():
     
@@ -6,6 +6,10 @@ def run_pipeline():
     
     print("Passo 1: Extraindo dados e gerando JSON")
     extrair_pokemons(151)
+
+    print("Passo 2: Extraindo especies e gerando JSON")
+    extrair_especie(151)
+
 
     print("--- PIPELINE FINALIZADA... POR ENQUANTO ---")
 
