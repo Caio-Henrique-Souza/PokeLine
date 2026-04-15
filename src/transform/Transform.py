7be2dde62d0f1820dba7042235bf369b2453b410
Caio Henrique Souza <detetives.l.c@gmail.com>
2026-04-15 08:22:39 -0300

    Extrair Status

--- src/transform/Transform.py
+++ src/transform/Transform.py
@@ -2,7 +2,7 @@ import pandas as pd
 import json
 import os
 
-def transformar_pokemon():
+def criar_dpokemon():
     origem = "data/raw/pokemons_detalhes_raw.json"
     destino = "data/processed"
     
@@ -27,3 +27,37 @@ def transformar_pokemon():
     df.to_csv(caminho_arquivo, index=False)
 
     print(f"✅ dPokemon criado com sucesso em {caminho_arquivo}")
+
+
+def criar_dpokestats():
+    origem = "data/raw/pokemons_detalhes_raw.json"
+    destino = "data/processed"
+    
+    os.makedirs(destino, exist_ok=True)
+
+    with open(origem, 'r') as f:
+        dados = json.load(f)
+
+    lista = []
+
+    for pokemon in dados:
+        lista.append({
+            "pokemon_id": pokemon["id"],
+            "hp": next(stat["base_stat"] for stat in pokemon["stats"] if stat["stat"]["name"] == "hp"),
+            "attack": next(stat["base_stat"] for stat in pokemon["stats"] if stat["stat"]["name"] == "attack"),
+            "defense": next(stat["base_stat"] for stat in pokemon["stats"] if stat["stat"]["name"] == "defense"),
+            "special-attack": next(stat["base_stat"] for stat in pokemon["stats"] if stat["stat"]["name"] == "special-attack"),
+            "special-defense": next(stat["base_stat"] for stat in pokemon["stats"] if stat["stat"]["name"] == "special-defense"),
+            "speed": next(stat["base_stat"] for stat in pokemon["stats"] if stat["stat"]["name"] == "speed")
+
+        })
+
+    df = pd.DataFrame(lista)
+
+    caminho_arquivo = f"{destino}/dPokeStats.csv"
+    df.to_csv(caminho_arquivo, index=False)
+
+    print(f"✅ dPokemon criado com sucesso em {caminho_arquivo}")
+
+
+criar_dpokestats()
