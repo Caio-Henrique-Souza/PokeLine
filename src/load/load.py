d47d3cc578d70bd28125dcb6fb1b1181fabd60c4
Caio Henrique Souza <detetives.l.c@gmail.com>
2026-04-15 17:25:46 -0300

    pipeline completo

--- src/load/load.py
+++ src/load/load.py
@@ -1,13 +1,26 @@
 import pandas as pd
-from sqlalchemy import create_engine
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine, text
+
+load_dotenv()
+
+USER = os.getenv("POSTGRES_USER")
+PASSWORD = os.getenv("POSTGRES_PASSWORD")
+HOST = os.getenv("POSTGRES_HOST")
+PORT = os.getenv("POSTGRES_PORT")
+DB = os.getenv("POSTGRES_DB")
 
 def carregar_moves():
     
     df = pd.read_csv("data/refined/dpokemoves_refined.csv", encoding="utf-8")
 
     engine = create_engine(
-        "postgresql+psycopg2://postgres:auth102@localhost:5432/pokelineDB"
+        f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB}"
     )
+    
+    with engine.begin() as conn:
+        conn.execute(text("TRUNCATE TABLE fact_pokemon_moves"))
 
     # 🚀 envia pro banco
     df.to_sql(
@@ -24,14 +37,16 @@ def carregar_pokemon():
     df = pd.read_csv("data/refined/dpokemon_refined.csv", encoding="utf-8")
 
     engine = create_engine(
-        "postgresql+psycopg2://postgres:auth102@localhost:5432/pokelineDB"
+        f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB}"
     )
+    
+    with engine.begin() as conn:
+        conn.execute(text("TRUNCATE TABLE fact_pokemon"))
 
-    # 🚀 envia pro banco
     df.to_sql(
         "fact_pokemon",
         engine,
-        if_exists="append",  # troca tabela toda
+        if_exists="append",
         index=False
     )
 
@@ -42,8 +57,11 @@ def carregar_pokeegg():
     df = pd.read_csv("data/refined/dpokeegg_refined.csv", encoding="utf-8")
 
     engine = create_engine(
-        "postgresql+psycopg2://postgres:auth102@localhost:5432/pokelineDB"
+        f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB}"
     )
+    
+    with engine.begin() as conn:
+        conn.execute(text("TRUNCATE TABLE fact_pokeegg"))
 
     # 🚀 envia pro banco
     df.to_sql(
@@ -60,8 +78,11 @@ def carregar_pokecharac():
     df = pd.read_csv("data/refined/dpokecharac_refined.csv", encoding="utf-8")
 
     engine = create_engine(
-        "postgresql+psycopg2://postgres:auth102@localhost:5432/pokelineDB"
+        f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB}"
     )
+    
+    with engine.begin() as conn:
+        conn.execute(text("TRUNCATE TABLE fact_pokecharac"))
 
     # 🚀 envia pro banco
     df.to_sql(
@@ -78,8 +99,12 @@ def carregar_pokestats():
     df = pd.read_csv("data/refined/dpokestats_refined.csv", encoding="utf-8")
 
     engine = create_engine(
-        "postgresql+psycopg2://postgres:auth102@localhost:5432/pokelineDB"
+        f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB}"
     )
+    
+    with engine.begin() as conn:
+        conn.execute(text("TRUNCATE TABLE fact_pokestats"))
+
 
     # 🚀 envia pro banco
     df.to_sql(
@@ -97,8 +122,11 @@ def carregar_poketype():
     df = pd.read_csv("data/refined/dpoketype_refined.csv", encoding="utf-8")
 
     engine = create_engine(
-        "postgresql+psycopg2://postgres:auth102@localhost:5432/pokelineDB"
+        f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB}"
     )
+    
+    with engine.begin() as conn:
+        conn.execute(text("TRUNCATE TABLE fact_poketype"))
 
     # 🚀 envia pro banco
     df.to_sql(
@@ -112,3 +140,7 @@ def carregar_poketype():
 
 carregar_pokemon()
 carregar_moves()
+carregar_pokecharac()
+carregar_pokeegg()
+carregar_pokestats()
+carregar_poketype()
